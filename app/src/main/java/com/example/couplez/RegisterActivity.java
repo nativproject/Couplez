8b943f92c5276edacfbbad982c728d115b765825
nativsibony <nativs@nucleon.sh>
2021-02-03 00:06:01 +0200

    done with images

--- app/src/main/java/com/example/couplez/RegisterActivity.java
+++ app/src/main/java/com/example/couplez/RegisterActivity.java
@@ -1,23 +1,33 @@
 package com.example.couplez;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -25,6 +35,10 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
     private Button register;
     private FirebaseAuth m_auth;
     private ProgressBar progress_bar;
+    private ImageView profile_img;
+    private StorageReference storage;
+    private Uri image_uri;
+    private boolean img_uploaded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +47,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         find_views();
         init_views();
         m_auth = FirebaseAuth.getInstance();
+        storage = FirebaseStorage.getInstance().getReference();
         progress_bar.bringToFront();
     }
 
@@ -43,9 +58,11 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         edit_password = (EditText) findViewById(R.id.password);
         register = (Button) findViewById(R.id.register);
         progress_bar = (ProgressBar) findViewById(R.id.register_progress_bar);
+        profile_img = (ImageView) findViewById(R.id.profile_img);
     }
 
     private void init_views() {
+        profile_img.setOnClickListener(this);
         register.setOnClickListener(this);
     }
 
@@ -55,12 +72,48 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             case R.id.register:
                 register_user();
                 break;
+            case R.id.profile_img:
+                set_profile_img();
+                img_uploaded = true;
+                break;
             default:
                 break;
         }
     }
 
-    private void register_user() {
+    public void set_profile_img() {
+        //open gallery
+        Intent open_gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(open_gallery, 200);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 200) {
+            if (resultCode == Activity.RESULT_OK) {
+                image_uri = data.getData();
+                profile_img.setImageURI(image_uri);
+            }
+        }
+    }
+
+    public void upload_img_to_firebase(Uri image_uri, String uid) {
+        StorageReference file_ref = storage.child(uid + ".jpg");
+        file_ref.putFile(image_uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(RegisterActivity.this, "Image uploaded successfully!", Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(RegisterActivity.this, "Failed to load image! Please try again!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void register_user() {
         String email = edit_email.getText().toString().trim();
         String age = edit_age.getText().toString().trim();
         String username = edit_username.getText().toString().trim();
@@ -99,6 +152,11 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             return;
         }
 
+        if (!img_uploaded) {
+            Toast.makeText(RegisterActivity.this, "Please select your profile picture!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
         progress_bar.setVisibility(View.VISIBLE);
         m_auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -113,6 +171,7 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                                 public void onComplete(@NonNull Task<Void> task) {
                                     if (task.isSuccessful()) {
                                         Toast.makeText(RegisterActivity.this, "User has been registered successfully!", Toast.LENGTH_LONG).show();
+                                        upload_img_to_firebase(image_uri, FirebaseAuth.getInstance().getCurrentUser().getUid());
                                         finishAffinity();
                                         startActivity(new Intent(RegisterActivity.this, MainActivity.class));
 
--- app/src/main/java/com/example/couplez/SwipeActivity.java
+++ app/src/main/java/com/example/couplez/SwipeActivity.java
@@ -3,30 +3,43 @@ package com.example.couplez;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class SwipeActivity extends AppCompatActivity implements View.OnClickListener {
+    private String TAG = "TAG";
     private Button btn_yes, btn_no;
     private TextView user_name, user_info;
     private ImageView user_image;
     private DatabaseReference users;
     private FirebaseAuth m_auth;
-    private String current_user;
-    private ArrayList<String> cards;
+    private StorageReference images;
+    private String current_uid;
+    private Stack<String> cards = new Stack<String>();
+    private ArrayAdapter arr_adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +48,12 @@ public class SwipeActivity extends AppCompatActivity implements View.OnClickList
         find_views();
         init_views();
         m_auth = FirebaseAuth.getInstance();
-        current_user = m_auth.getCurrentUser().getUid();
+        current_uid = m_auth.getCurrentUser().getUid();
 
-        display_couples(current_user);
+        display_cards(current_uid);
+
+//        arr_adapter = new ArrayAdapter<>(this, R.layout.item, R.hello, cards);
+
     }
 
     private void find_views() {
@@ -53,18 +69,18 @@ public class SwipeActivity extends AppCompatActivity implements View.OnClickList
         btn_yes.setOnClickListener(this);
     }
 
-    public void display_couples(String current_user) {
+    public void display_cards(String current_uid) {
         users = FirebaseDatabase.getInstance().getReference().child("Users");
         users.addValueEventListener(new ValueEventListener() {
 
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                cards = new ArrayList<>();
                 for (DataSnapshot child : snapshot.getChildren()) {
                     // TODO: 02/02/2021 Add check if this current user exist in other users "no" branch
-                    if (!current_user.equalsIgnoreCase(child.getKey()))
-                        cards.add(child.getKey());
+                    if (!current_uid.equalsIgnoreCase(child.getKey()))
+                        cards.push(child.getKey());
                 }
+                display_couple(cards.pop());
             }
 
             @Override
@@ -75,23 +91,41 @@ public class SwipeActivity extends AppCompatActivity implements View.OnClickList
         });
     }
 
+    public void display_couple(String card) {
+        images = FirebaseStorage.getInstance().getReference(card + ".jpg");
+        final long ONE_MEGABYTE = 1024 * 1024;
+        images.getBytes(ONE_MEGABYTE)
+                .addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                    @Override
+                    public void onSuccess(byte[] bytes) {
+                        Bitmap bm = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                        user_image.setImageBitmap(bm);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+            }
+        });
+    }
+
+    // TODO: 14/12/2020 update database with the current user decision
+    private void update_db(String decision) {
+
+    }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_yes:
-//                update_db("yes");
-                Log.d("aaa", "hello");
+                update_db("yes");
                 break;
             case R.id.btn_no:
-//                update_db("no");
-                Log.d("aaa", "bitch");
+                update_db("no");
                 break;
             default:
                 break;
         }
     }
 
-//    private void update_db(String ans) {
-//        //// TODO: 14/12/2020 update database with the current user decision
-//    }
+
 }
