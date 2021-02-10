a8bbfac4a61503175632c75854bba226101be2cc
nativsibony <nativs@nucleon.sh>
2021-02-10 15:55:34 +0200

    finally fixed the stack issue

--- app/src/main/java/com/example/couplez/SwipeActivity.java
+++ app/src/main/java/com/example/couplez/SwipeActivity.java
@@ -5,9 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
@@ -24,9 +21,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Stack;
 
 public class SwipeActivity extends AppCompatActivity implements View.OnClickListener {
@@ -39,8 +33,7 @@ public class SwipeActivity extends AppCompatActivity implements View.OnClickList
     private String current_uid, TAG = "TAG";
     public Stack<String> users_ids = new Stack<String>();
     public Stack<User> users_info = new Stack<User>();
-    public User current_user = null;
-    public boolean finished = false;
+    public boolean finished = false, is_child_exist = false, is_equal_current = false; // FLAGS
 
 
     @Override
@@ -49,17 +42,39 @@ public class SwipeActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_swipe);
         find_views();
         init_views();
-        Log.d(TAG, "onCreate");
+        Log.d(TAG, "ON CREATE");
         m_auth = FirebaseAuth.getInstance();
         current_uid = m_auth.getCurrentUser().getUid();
         progress_bar.bringToFront();
 
-        read_data(current_uid, () -> {
-            finished = true;
-            current_user = users_info.pop();
-            init_couples(current_user);
-            progress_bar.setVisibility(View.GONE);
+        read_data(current_uid, new first_callback() {
+            @Override
+            public void onCallback() {
+                Log.d(TAG, "------------------------------1st CALLBACK------------------------------");
+                if (!finished) {
+                    print_users();
+                    btn_no.setClickable(true);
+                    btn_yes.setClickable(true);
+                    finished = true;
+                    if (!users_info.isEmpty()) {
+                        display_couple(users_info.peek());
+                        users_info.pop();
+                    } else {
+                        show_empty_screen();
+                    }
+                    progress_bar.setVisibility(View.GONE);
+                }
+            }
         });
+    }
+
+    private void print_users() {
+        int i = 1;
+        for (User user : users_info) {
+            Log.d(TAG, i + " " + user.toString());
+            Log.d(TAG, "UID: " + users_ids.get(i - 1));
+            i++;
+        }
     }
 
     public Bitmap str_to_bitmap(String encodedString) {
@@ -88,26 +103,33 @@ public class SwipeActivity extends AppCompatActivity implements View.OnClickList
         btn_yes.setOnClickListener(this);
     }
 
-    private void read_data(String current_uid, firebase_callback callback) {
-        Log.d(TAG, "read_data:                              .");
+    private void read_data(String current_uid, first_callback callback) {
+        btn_no.setClickable(false);
+        btn_yes.setClickable(false);
+
         all_users = FirebaseDatabase.getInstance().getReference().child("Users");
         all_users.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot child : snapshot.getChildren()) {
+                if (!finished) {
+                    Log.d(TAG, "CURRENT  USER " + current_uid);
+                    for (DataSnapshot child : snapshot.getChildren()) {
+                        is_child_exist = exist_in_decision(child);
+                        is_equal_current = equal_to_current_user(child, current_uid);
+                        if (!is_equal_current && !is_child_exist) {
+                            users_ids.push(child.getKey()); // Users ID stack
+                            String profile_image = child.child("profile_image").getValue().toString();
+                            String your_age = child.child("ages").child("your_age").getValue().toString();
+                            String partner_age = child.child("ages").child("partner_age").getValue().toString();
+                            String your_name = child.child("names").child("your_name").getValue().toString();
+                            String partner_name = child.child("names").child("partner_name").getValue().toString();
+                            String email = child.child("email").getValue().toString();
+                            String about = child.child("about").getValue().toString();
 
-                    if (!current_uid.equalsIgnoreCase(child.getKey()) && !child.child("no").getKey().equals(current_uid)) {//Add this late => && child.child("no").getKey().equals(current_uid)
-                        users_ids.push(child.getKey());//Create stack with all potential uid's
-                        String profile_image = child.child("profile_image").getValue().toString();
-                        String your_age = child.child("ages").child("your_age").getValue().toString();
-                        String partner_age = child.child("ages").child("partner_age").getValue().toString();
-                        String your_name = child.child("names").child("your_name").getValue().toString();
-                        String partner_name = child.child("names").child("partner_name").getValue().toString();
-                        String email = child.child("email").getValue().toString();
-                        String about = child.child("about").getValue().toString();
-
-                        User user = new User(email, your_name, your_age, partner_name, partner_age, profile_image, about);
-                        users_info.push(user); //Create stack with all uid's info
+                            User user = new User(email, your_name, your_age, partner_name, partner_age, profile_image, about);
+                            users_info.push(user); // Users information Stack
+                        }
+                        is_child_exist = false;
                     }
                 }
                 callback.onCallback();
@@ -119,12 +141,30 @@ public class SwipeActivity extends AppCompatActivity implements View.OnClickList
         });
     }
 
-    private interface firebase_callback {
+    private boolean equal_to_current_user(DataSnapshot child, String current_uid) {
+        String c_uid = child.getKey();
+        if (c_uid.equals(current_uid))
+            return true;
+        return false;
+    }
+
+    // Check if decision has made
+    // and if child exist in decision
+    public boolean exist_in_decision(DataSnapshot child) {
+        if (child.hasChild("decision"))
+            if (child.child("decision").hasChild("dislike") && child.child("decision").child("dislike").hasChild(current_uid))
+                return true;
+            else if (child.child("decision").hasChild("like") && child.child("decision").child("like").hasChild(current_uid))
+                return true;
+        return false;
+    }
+
+    // simple callback function
+    private interface first_callback {
         void onCallback();
     }
 
-    public void init_couples(User current_user) {
-        print_stack(users_info);
+    public void display_couple(User current_user) {
         Bitmap decode = str_to_bitmap(current_user.profile_image);
         user_image.setImageBitmap(decode);
         user_name.setText(current_user.your_name + " & " + current_user.partner_name);
@@ -134,33 +174,35 @@ public class SwipeActivity extends AppCompatActivity implements View.OnClickList
 
     // TODO: 14/12/2020 update database with the current user decision
     public void user_selection(String decision, String current_uid) {
-        Log.d(TAG, "user_selection: " + decision);
+        Log.d(TAG, "------------------------------USER SELECTION: " + decision + "------------------------------");
         try {
-            update_db(decision, users_ids.pop(), current_uid, () -> {
-                if (!users_info.empty()) {
-                    current_user = users_info.pop();
-                    init_couples(current_user);
-                } else
-                    show_empty_screen(); // TODO: 04/02/2021 Show screen that indicates that no users are left to show
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
+            update_db(decision, users_ids.pop(), current_uid);
+            Log.d(TAG, "------------------------------END DATABASE UPDATE------------------------------");
+            if (!users_info.empty()) {
+                btn_no.setClickable(true);
+                btn_yes.setClickable(true);
+                print_users();
+                Log.d(TAG, "------------------------------END 2nd PRINT------------------------------");
+                display_couple(users_info.peek());
+                users_info.pop();
+            } else {
+                btn_no.setClickable(false);
+                btn_yes.setClickable(false);
+                show_empty_screen();
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
         }
     }
 
-    private void print_stack(Stack<User> users) {
-        for (User user : users) {
-            Log.d(TAG, "print_stack: " + user.toString());
-        }
-    }
-
-    public void update_db(String decision, String user_id, String current_uid, firebase_callback callback) {
+    public void update_db(String decision, String user_id, String current_uid) {
+        btn_no.setClickable(false);
+        btn_yes.setClickable(false);
         all_users = FirebaseDatabase.getInstance().getReference().child("Users");
         all_users.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 all_users.child(user_id).child("decision").child(decision).child(current_uid).setValue(true);
-                callback.onCallback();
             }
 
             @Override
@@ -171,9 +213,12 @@ public class SwipeActivity extends AppCompatActivity implements View.OnClickList
     }
 
     public void show_empty_screen() {
-        user_image.setImageDrawable(getResources().getDrawable(R.drawable.sorry));
         btn_no.setClickable(false);
         btn_yes.setClickable(false);
+        user_image.setImageDrawable(getResources().getDrawable(R.drawable.sorry));
+        user_age.setText("");
+        user_name.setText("");
+        user_about.setText("");
     }
 
     @Override
@@ -181,14 +226,20 @@ public class SwipeActivity extends AppCompatActivity implements View.OnClickList
         switch (v.getId()) {
             case R.id.btn_yes:
                 if (finished)
-                    user_selection("yes", current_uid);
+                    user_selection("like", current_uid);
                 break;
             case R.id.btn_no:
                 if (finished)
-                    user_selection("no", current_uid);
+                    user_selection("dislike", current_uid);
                 break;
             default:
                 break;
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
